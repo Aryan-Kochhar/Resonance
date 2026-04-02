dc311078531bcdad552e1605e748e92778777a7f
Etrlnx <samanyugupta465@gmail.com>
2026-04-02 16:26:59 +0530

    Made a folder to store the scripts again

--- data_check.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-indoor = np.load('channels_i2_28b.npy')
-outdoor = np.load('channels_o1_60.npy', allow_pickle=True)
-combined = np.load('channels_combined.npy', allow_pickle=True).item()
-
-print("Indoor shape:", indoor.shape)
-print("Outdoor length:", len(outdoor))
-print("Combined keys:", combined.keys())
