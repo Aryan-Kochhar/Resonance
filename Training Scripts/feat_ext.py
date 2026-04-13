fa6027dae1040e6167868175d35761276638bcca
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-04-13 11:55:44 +0530

    remove old training scripts

--- Training Scripts/feat_ext.py
+++
@@ -1,9 +0,0 @@
-# Load preprocessed dataset
-outdoor_pre = np.load('channels_o1_60_preprocessed.npy')
-
-# Extract amplitude and phase
-amplitude = np.abs(outdoor_pre)
-phase = np.angle(outdoor_pre)
-
-print("Amplitude shape:", amplitude.shape)
-print("Phase shape:", phase.shape)
